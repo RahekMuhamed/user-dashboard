d7c089ad11526c747b4219f43a6e6813a100047a
RahekMuhamed <raheksalem@gmail.com>
2024-08-07 08:35:56 +0300

    add the routing

---
+++ user-dashboard/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+
+export const routes: Routes = [
+    { path: '', component: UserListComponent },
+    { path: 'user-list', component: UserListComponent },
+
+  { path: 'user/:id', component: UserDetailComponent }
+];
+
